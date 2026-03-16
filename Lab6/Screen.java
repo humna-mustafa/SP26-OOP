c0f9e64c0383ef8bedfe37f8a8aac57f28caf24f
Humna Mustafa <humnamustafa01@gmail.com>
2026-03-16 22:28:22 +0500

    creen class with rows of seats and different seat types

--- Lab6/Screen.java
+++ Lab6/Screen.java
@@ -4,39 +4,40 @@ class Screen {
     Movie movie;
     Seat[][] seats;
 
-    Screen(String id,String type,int rows,int maxCols,Movie m) {
+Screen(String id,String type,int rows,int maxCols,Movie m) {
         screenId=id;
         screenType=type;
         movie=m;
         seats=new Seat[rows][];
 
         int seatNo=1;
-        for(int i=0;i<rows;i++) {
+  for(int i=0;i<rows;i++) {
             int cols=maxCols-i;
             if(cols<1) cols=1;
             seats[i]=new Seat[cols];
 
-            for(int j=0;j<cols;j++) {
-                String typeSeat="Regular";
-                double price=500;
-                if(i>rows/2) {
-                    typeSeat="Premium";
-                    price=800;
+
+    for(int j=0;j<cols;j++) {
+         String typeSeat="Regular";
+          double price=500;
+            if(i>rows/2) {
+                typeSeat="Premium";
+                 price=800;
                 }
-                if(i==rows-1) {
-                    typeSeat="VIP";
-                    price=1200;
+          if(i==rows-1) {
+                typeSeat="VIP";
+                 price=1200;
                 }
                 seats[i][j]=new Seat(seatNo,i+1,typeSeat,price);
                 seatNo++;
-            }
-        }
+            } }
     }
+
 
     void showSeats() {
         for(int i=0;i<seats.length;i++) {
             for(int j=0;j<seats[i].length;j++) {
-                System.out.print(seats[i][j].seatId + " ");
+               System.out.print(seats[i][j].seatId + " ");
             }
             System.out.println();
         }
